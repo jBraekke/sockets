19cef402a9cb7aad78879069281da0799fc17707
Jørg1 Brække <jorgenhb@live.com>
2019-09-30 14:55:54 +0200

    endring av socket

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var path = require("path");
 var io = require('socket.io')(server);
 
 const users = [];
+const list = [];
+const statuser = ['KLARGJORT', 'LOGGET_PAA', 'STARTET', 'INNSYN_PAAGAAR', 'AVSLUTTET']
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,24 +19,42 @@ server.listen(app.get('port'), function () {
 });
 
 app.get('/', function (request, response) {
-  response.sendFile((path.join(__dirname+'/index.html')));
+  response.sendFile((path.join(__dirname + '/index.html')));
 });
 
 app.get('/melder', function (request, response) {
-  response.sendFile((path.join(__dirname+'/public/melder.html')));
+  response.sendFile((path.join(__dirname + '/public/melder.html')));
 });
 
 io.on('connection', function (socket) {
-    socket.emit('all-users', users);
+  socket.emit('all-users', users);
 
-    socket.on('new-user', user => {
-        users.push(user);
-        io.emit('new-user-emitted', users);
-    });
-
-    socket.on('login', user => {
-      users.filter((element) => { return element.user === user.user})[0].aktiv = true
-      io.emit('new-user-emitted', users);
-    })
-
+  socket.on('new-user', user => {
+    users.push(user);
+    io.emit('new-user-emitted', users);
   });
+
+  socket.on('login', user => {
+    users.filter((element) => { return element.user === user.user })[0].aktiv = true
+    io.emit('new-user-emitted', users);
+  })
+
+  socket.on('new-list', items => {
+    if (list.length === 0) {
+      items.forEach(element => {
+        list.push(element);
+      });
+    }
+  });
+
+  setInterval(() => {
+    if (list.length > 0) {
+      const status = statuser[Math.floor(Math.random() * statuser.length)];
+      const item = list[Math.floor(Math.random() * list.length)];
+      item.status = status;
+
+      socket.emit('all-list', list);
+    }
+  }, 1000)
+
+});
