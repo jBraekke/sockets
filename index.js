d2239e78ffd8cde539c2a54550631a96b9f3c530
Brakke <jorgen.brakke@capgemini.com>
2020-09-09 10:10:52 +0200

    Bytting av prosjekt

--- index.js
+++ index.js
@@ -3,34 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var path = require("path");
-var io = require('socket.io')(server);
-var schedule = require('node-schedule');
-var fetch = require('node-fetch');
 
-const users = [];
-const list = [];
-
-function requestOptions() {
-  return {
-      headers: {
-          'Authorization': 'Bearer svv_oppgaveutvikler',
-          'Content-Type': 'application/json'
-      },
-      credentials: 'include',
-      method: 'GET',
-  }
-}
-
-
-async function hentPaagaende() {
-
-  const response = await fetch('http://localhost:8088/backend/api/prove/paagaaende', requestOptions);
-
-  const data = await response.json();
-
-  return data;
-
-}
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -42,39 +15,12 @@ server.listen(app.get('port'), function () {
 });
 
 app.get('/', function (request, response) {
-  response.sendFile((path.join(__dirname + '/index.html')));
-});
 
-app.get('/melder', function (request, response) {
-  response.sendFile((path.join(__dirname + '/public/melder.html')));
-});
-
-io.on('connection', function (socket) {
-  schedule.scheduleJob('*/2 * * * * *', async function () {
-    const liste = await hentPaagaende();
-    socket.emit('all-list', liste);
-  });
-
-  socket.on('new-user', user => {
-    users.push(user);
-    io.emit('new-user-emitted', users);
-  });
-
-  socket.on('login', user => {
-    users.filter((element) => { return element.user === user.user })[0].aktiv = true
-    io.emit('new-user-emitted', users);
-  })
-
-  socket.on('new-list', items => {
-    if (items.length > 0) {
-
-      while (list.length > 0) {
-        list.pop();
-      }
-
-      items.forEach(element => {
-        list.push(element);
-      });
-    }
-  });
+  console.log(request.query.code, 'hi')
+  if (request.query.code === "1337") {
+    response.sendFile((path.join(__dirname + '/public/index2.html')));
+  }
+  else {
+    response.send('Nei, feil kode. Prøv igjen');
+  }
 });
---
+++ public/precache-manifest.536d4ae80c17cd7204eed6f269c444ec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d23acf37bc308e5b3b5f74101fe648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbc46e28e7f883e0a886",
+    "url": "/static/css/main.aae54a8f.chunk.css"
+  },
+  {
+    "revision": "916958e6c295d6117dc3",
+    "url": "/static/js/2.0cc441a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0cc441a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc46e28e7f883e0a886",
+    "url": "/static/js/main.e10e6a7f.chunk.js"
+  },
+  {
+    "revision": "d693b0fa7aede6b69172",
+    "url": "/static/js/runtime-main.f0524b44.js"
+  },
+  {
+    "revision": "ac99f1c40e01ad9239c90854b3f12942",
+    "url": "/static/media/bro.ac99f1c4.png"
+  },
+  {
+    "revision": "d9ae61be43cbf1835a8c6530eae2e543",
+    "url": "/static/media/bro2.d9ae61be.png"
+  },
+  {
+    "revision": "7eb56288c61ebfac1479b0648e00b5ce",
+    "url": "/static/media/logo.7eb56288.svg"
+  },
+  {
+    "revision": "8b844a0e8550a0bca04b36d07bf48208",
+    "url": "/static/media/map.8b844a0e.png"
+  }
+]);
